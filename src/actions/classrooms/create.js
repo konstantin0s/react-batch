090712f45cde4784b1d71900a0ac15fb40f1a897
konstantin0s <constantintofan85@gmail.com>
2018-01-25 18:23:04 +0100

    Update actions/ create classroom, not working yet

--- src/actions/classrooms/create.js
+++ src/actions/classrooms/create.js
@@ -1,4 +1,38 @@
-// src/actions/classrooms/create.js
+// // src/actions/classrooms/create.js
+//
+// import API from '../../api/client'
+// import {
+//   APP_LOADING,
+//   APP_DONE_LOADING,
+//   LOAD_ERROR,
+//   LOAD_SUCCESS
+// } from '../loading'
+//
+// const api = new API()
+//
+// export const CLASSROOM_CREATED = 'CLASSROOM_CREATED'
+//
+// export const createClassroom = (newClassroom) => {
+//   return (dispatch) => {
+//     dispatch({ type: APP_LOADING })
+//
+//     api.post('/classrooms', newClassroom)
+//       .then(() => {
+//         dispatch({ type: APP_DONE_LOADING })
+//         dispatch({ type: LOAD_SUCCESS })
+//       })
+//       .catch((error) => {
+//         dispatch({ type: APP_DONE_LOADING })
+//         dispatch({
+//           type: LOAD_ERROR,
+//           payload: error.message
+//         })
+//       })
+//   }
+// }
+
+
+
 
 import API from '../../api/client'
 import {
@@ -10,13 +44,11 @@ import {
 
 const api = new API()
 
-export const CLASSROOM_CREATED = 'CLASSROOM_CREATED'
-
-export const createClassroom = (newClassroom) => {
+ export const createClassroom = (newClassroom) => {
   return (dispatch) => {
     dispatch({ type: APP_LOADING })
 
-    api.post('/classrooms', newClassroom)
+    api.post('/classrooms', {})
       .then(() => {
         dispatch({ type: APP_DONE_LOADING })
         dispatch({ type: LOAD_SUCCESS })
